7e005904b48e1958bbc035f38c9fad154fd8508c
Willian Souza <willian.souza@tooseguros.com.br>
2022-12-07 08:41:44 -0300

    add grpc-svc

--- apps/grpc-svc/src/app.module.ts
+++ apps/grpc-svc/src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { CustomersModule } from './contexts/customers/customers.module';
+import { DatabaseModule } from '@app/common/database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        CONNECTION_TYPE: Joi.string().required(),
+        CONNECTION_STRING: Joi.string().required(),
+      }),
+      envFilePath: './apps/grpc-svc/.env',
+    }),
+    DatabaseModule,
+    CustomersModule,
+  ],
   controllers: [],
   providers: [],
 })
--- apps/grpc-svc/src/main.ts
+++ apps/grpc-svc/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions } from '@nestjs/microservices';
+import { grpcSvcClient } from '../../../libs/common/src';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    grpcSvcClient,
+  );
+  await app.listen();
 }
 bootstrap();
